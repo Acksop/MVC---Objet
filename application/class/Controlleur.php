2747e20c4d19645c9b3a7aba9fefa11dca172e0f
Emmanuel ROY <ornithorynxe@live.fr>
2020-04-30 06:59:41 +0200

    Correction majuscule Controlleur

--- application/class/Controlleur.php
+++ application/class/Controlleur.php
@@ -18,8 +18,8 @@ class Controlleur{
             $url_params = $application->url->page['params'];
 	        require TRAITEMENT_PATH . DIRECTORY_SEPARATOR . $application->url->page['name'] . '.php';
         } else {
-            $this->modele = new modele($application->url->page);
-            $this->vue = new vue($this);
+            $this->modele = new Modele($application->url->page);
+            $this->vue = new Vue($this);
         }
 	}
 	
