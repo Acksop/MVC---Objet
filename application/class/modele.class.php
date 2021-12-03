723772fc98af75f0bd9573cdf1862a6f0d63180a
Emmanuel ROY <emmanuel.roy@infoartsmedia.fr>
2021-12-03 11:01:52 +0100

    Update modele.class.php

--- application/class/modele.class.php
+++ application/class/modele.class.php
@@ -9,11 +9,11 @@ class Modele{
 		if(file_exists(MODELS_PATH.DIRECTORY_SEPARATOR.$base_param['name'].'.model')){
 			$fichier = file(MODELS_PATH.DIRECTORY_SEPARATOR.$base_param['name'].'.model');
 			foreach ($fichier as $ligne_num => $ligne) {
-                if (preg_match("#[ ]*([a-zA-Z-_+]*)[ ]*[:][ ]*([a-zA-Z-_+ ']*[ ]*)#", $ligne, $matches)) {
-                    $this->page[$matches[1]] = $matches[2];
-                }
-            }
-            $this->page['url_params'] = $base_param['params'];
+                		if (preg_match("#[ ]*([a-zA-Z-_+]*)[ ]*[:][ ]*([a-zA-Z-_+ ']*[ ]*)#", $ligne, $matches)) {
+                    			$this->page[$matches[1]] = $matches[2];
+                		}
+            		}
+            		$this->page['url_params'] = $base_param['params'];
 		}else{
 			$this->page['name'] = $base_param['name'];
 			$this->page['description'] = $base_param['description'];
