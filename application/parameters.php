14330dcf7c35443bde8ac1b35bd0c2ad7c0c5599
Emmanuel ROY <emmanuel.roy@infoartsmedia.fr>
2021-12-03 10:31:10 +0100

    Update parameters.php

--- application/parameters.php
+++ application/parameters.php
@@ -8,4 +8,4 @@ define('DSN_BDD2',"mysql:host=localhost;dbname=db2");
 define('USER_BDD2',"user2");
 define('PASS_BDD2',"pass2");
 
-define('PATH_URL',"http://vlan.localhost/");
+define('PATH_URL',"http://localhost/");
