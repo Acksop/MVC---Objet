08a01b2d767fa8e886a837cf8897e091175be7fe
Emmanuel ROY <emmanuel.roy@infoartsmedia.fr>
2021-12-03 10:17:29 +0100

    Update parameters.php

--- application/parameters.php
+++ application/parameters.php
@@ -1,11 +1,11 @@
 <?php
 
-define('DSN_FICHES',"mysql:host=localhost;dbname=db1");
-define('USER_FICHES',"user1");
-define('PASS_FICHES',"pass1");
+define('DSN_BDD1',"mysql:host=localhost;dbname=db1");
+define('USER_BDD1',"user1");
+define('PASS_BDD1',"pass1");
 
-define('DSN_ABITOP',"mysql:host=localhost;dbname=db2");
-define('USER_ABITOP',"user2");
-define('PASS_ABITOP',"pass2");
+define('DSN_BDD2',"mysql:host=localhost;dbname=db2");
+define('USER_BDD2',"user2");
+define('PASS_BDD2',"pass2");
 
 define('PATH_URL',"http://ssdom.domain.tld/public/");
