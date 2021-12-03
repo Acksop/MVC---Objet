b4ef9d3e54dc87bb6ce522b6b4f9bc7a74d83b76
Emmanuel ROY <emmanuel.roy@infoartsmedia.fr>
2021-12-03 11:00:13 +0100

    Update controlleur.class.php

--- application/class/controlleur.class.php
+++ application/class/controlleur.class.php
@@ -15,13 +15,12 @@ class Controlleur{
 	
 	public function __construct($application){
 
-	    if($application->url->page['control']){
-            $url_params = $application->url->page['params'];
-	        require TRAITEMENT_PATH . DIRECTORY_SEPARATOR . $application->url->page['name'] . '.php';
-        } else {
-            $this->modele = new Modele($application->url->page);
-            $this->vue = new Vue($this);
-        }
+		if($application->url->page['control']){
+            		$url_params = $application->url->page['params'];
+	        	require TRAITEMENT_PATH . DIRECTORY_SEPARATOR . $application->url->page['name'] . '.php';
+            	} else {
+            		$this->modele = new Modele($application->url->page);
+			$this->vue = new Vue($this);
+		}
 	}
-	
 }
