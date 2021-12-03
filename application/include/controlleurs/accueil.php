e35cbd211f69120aa907b47503b6e4c0395a9adb
Emmanuel ROY <emmanuel.roy@infoartsmedia.fr>
2021-12-03 11:19:07 +0100

    Update accueil.php

---
+++ application/include/controlleurs/accueil.php
@@ -0,0 +1,24 @@
+/**
+ * CONTROLLEUR STRUCTUREL FONCTIONNEL
+ *
+ * Nom Fichier: Acceuil
+ * Auteur:	Emmanuel ROY <acksop@yahoo.fr>
+ *
+ *
+ * Description developpeur:
+ * 
+ * C'est dans cette partie que l'on créé les fonctions php qui seront appelées par la vue
+ *  ce qui créé de ce fait un échange entre les deux fichiers.
+ *  
+ *  Celles ci peuvent contenir un ou plusieur accesseurs / inserteurs / modifieurs 
+ *  localisés dans les "connecteurs" de la base de données, ceux-ci étants 
+ *  définis par une requète à la syntaxe SQL correspondant au nom du fichier
+ *  et ses commentaires descriptifs *    
+ *  
+ *
+ * Description utilisateur:
+ *
+ *
+ *
+ *
+ */
