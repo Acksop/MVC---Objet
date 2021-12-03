816860a233e70c30054b48d4cc8be9abb6c98e1f
Emmanuel ROY <emmanuel.roy@infoartsmedia.fr>
2021-12-03 11:08:58 +0100

    Update tri.class.php

--- application/class/tri.class.php
+++ application/class/tri.class.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: roy
+ * Created by PhpStorm by usage of https://php.net.
+ * User: Emmanuel ROY
  * Date: 05/03/2019
  * Time: 16:08
  */
 class Tri{
 
     public static function cmp($a,$b){
-        if ($a == $b) {
+        if( $a == $b ){
             return 0;
         }
         return ($a < $b) ? -1 : 1;
